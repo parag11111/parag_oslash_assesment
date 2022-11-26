71d114fc8810afd473cc48b907b7b62a3100191c
parag11111 <100030196+parag11111@users.noreply.github.com>
2022-11-26 23:42:47 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+python -m parag_oslash sample_input\input1.txt
+python -m parag_oslash sample_input\input2.txt
+python -m parag_oslash sample_input\input3.txt
+python -m parag_oslash sample_input\input4.txt
